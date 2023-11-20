9e4362961fa182f2789bbd2eaaa9eeb92548caef
ANHAR DHIMAN <anhardhiman0000@users.noreply.github.com>
2023-11-20 17:52:02 +0530

    code push 2
    update the code in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 import Weather from "./Components/Weather";
+import './App.css'
+import './text.css'
 
 function App() {
   return (
-    <Weather />
+    <>
+      <div className='App'>
+        <h1 className='animate-charcter'>Weather Application</h1> 
+        <br/>
+        <h4 className='animate-charcter'>Created by: Anhar Dhiman</h4>
+      </div>
+      <Weather />
+    </>
   );
 }
 
